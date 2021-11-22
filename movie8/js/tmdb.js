72e03c53db71873503ddc6a1d2ef997524fbaa95
yoursecretarea <cpricilia01@gmail.com>
2021-11-22 16:47:31 +0800

    update movie8

--- movie8/js/tmdb.js
+++ movie8/js/tmdb.js
@@ -1,8 +1,10 @@
 const key = 'c3b60b13e7c85e386cdf467ef64b5c91'; //sesuai API Key Anda
 const endpoint_url = 'https://api.themoviedb.org/3/';
 
-function getListMovie() {
-    fetch(endpoint_url + "movie/now_playing?api_key=" + key + "&language=en-US&page=1")
+
+function getListMovie(services, sectiontitle) {
+    // fetch untuk dapat data dari server API penyedia
+    fetch(endpoint_url + services + "?api_key=" + key + "&language=en-US&page=1")
     .then(status)
     .then(json)
     .then(function(data) {
@@ -11,23 +13,27 @@ function getListMovie() {
 
     var moviesHTML = "";
     data.results.forEach(function(movie) {
-    moviesHTML += `
+    moviesHTML =  moviesHTML + `
         <div class="col m3 s6">
-            <div class="card">
+            <div class="card moviecard">
                 <a href="./movie.html?id=${movie.id}">
                     <div class="card-image waves-effect waves-block waves-light">
                         <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" />
                     </div>
                 </a>
                 <div class="card-content text-center">
-                    <strong>${movie.title}</strong>
+                    <strong>${movie.title}</strong><br>
+                    <strong>Release Date : ${movie.release_date}</strong>
                 </div>
+
             </div>
         </div>
         `;
     });
 
     document.getElementById("movie_list").innerHTML = moviesHTML;
+    document.getElementById("section_title").innerHTML = sectiontitle;
+
     })
     .catch(error);
 }
